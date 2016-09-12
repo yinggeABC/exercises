e1e26fc908fb4bd755c493008be259bb9a5bb8c5
yinggeABC <906772533@qq.com>
2016-09-13 00:17:30 +0800

    add simple backend

--- CRUD/client/client.js
+++ CRUD/client/client.js
@@ -1,3 +1,15 @@
 /**
  * Created by MWei2 on 9/12/2016.
  */
+$(function(){
+    var addBtn = $(".add");
+    var name = $("#name").val("");//开始的时候清空输入框
+    var age = $("#age").val("");
+    addBtn.on("click",function(){
+        name = $("#name").val();
+        age = $("#age").val();
+        $.ajax({
+            
+        })
+    })
+})
--- CRUD/server.js
+++ CRUD/server.js
@@ -1,3 +1,21 @@
 /**
  * Created by MWei2 on 9/12/2016.
  */
+var express = require("express")
+var app = express()
+var bodyParser = require("body-parser");
+var fs = require("fs")
+app.use(bodyParser.json());
+app.post("/addUser",function(req,res){
+    var user  = req.body;
+    var users = fs.readFile("./user.json","utf8");
+    users = JSON.parse(users);//转成JSON对象
+    var exists = users.every(function(item){
+        return (item.name!=user.name)&&(item.age!=user.age);
+    })
+    if(exists) users.push(user);
+    fs.writeFile("user.json",users);
+    res.send(JSON.stringify(users));//发送JSON类型字符串
+})
+
+app.listen(8080)
