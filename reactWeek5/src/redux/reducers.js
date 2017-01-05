3c8acb6fa9ecf277a27e0d196758743d0d44c3c0
yinggeABC <906772533@qq.com>
2017-01-05 09:00:53 +0800

    simple calculator

--- reactWeek5/src/redux/reducers.js
+++ reactWeek5/src/redux/reducers.js
@@ -2,16 +2,15 @@
  * Created by weimin on 17-1-4.
  */
 
-const counter =(count=0,action)=>{
-
+const calculator = ({add1:0,add2:0,purpose:"INPUT",res:0,sel:0},action)=>{
+    let state = {add1,add2,purpose,res,sel};
+    let sel=0;
     switch (action.type){
-        case "Inc":
-            return count+1;
-        case "Dec":
-            return count-1;
-        default:
-            return count;
+        case "INPUT":
+          return  Object.assign(state,{purpose:"INPUT"});
+        case "SELECT":
+          if (state.sel==0)
     }
 }
 
-export default counter;
+export default calculator;
